4ef982a78fe1b7642695b1b08f1e926435fcf859
OlenaDoroshenko <70623973+OlenaDoroshenko@users.noreply.github.com>
2022-08-24 19:15:00 +0300

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -33,6 +33,7 @@ insertListItems(listItemMarkup);
 // simpleLigthBox
 
 import SimpleLightbox from "simplelightbox";
+
 import "simplelightbox/dist/simple-lightbox.min.css";
 
 let gallery = new SimpleLightbox('.gallery a', {
